06a1551f2de7c622531637f89fa47f2e31b130a8
sebapinery <sebapinery@gmail.com>
2021-08-14 19:18:07 -0300

    feat/openweather service created

--- src/modules/cities/open-weather/open-weather.service.ts
+++ src/modules/cities/open-weather/open-weather.service.ts
@@ -2,4 +2,13 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
-export class OpenWeatherService {}
+export class OpenWeatherService {
+  async getForecastByCityName(cityName: string): Promise<any> {
+    const apiKey = '9d572bdbde5fb9601ddb90ddc16a0053';
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
+      { headers: { 'Content-Type': 'application/json' } },
+    );
+    return data;
+  }
+}

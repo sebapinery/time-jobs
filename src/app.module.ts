95ce2a6f9df126d0b9e5d44754cef7cd96d757de
sebapinery <sebapinery@gmail.com>
2021-08-14 17:23:21 -0300

    enviroment variables added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(
-      'mongodb://root:pass12345@mongodb:27017/clim-app?authSource=admin',
+      `mongodb://${process.env.ME_CONFIG_MONGODB_ADMINUSERNAME}:${process.env.ME_CONFIG_MONGODB_ADMINPASSWORD}@${process.env.ME_CONFIG_MONGODB_SERVER}:${process.env.ME_CONFIG_MONGODB_PORT}/${process.env.ME_CONFIG_MONGODB_NAME}?authSource=admin`,
     ),
   ],
   controllers: [AppController],

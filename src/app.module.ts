a9faa9c59dd24801b8c37aa2f235604f4144b3e6
sebapinery <sebapinery@gmail.com>
2021-08-14 22:22:24 -0300

    feat/set OpenWeather api key as env-variable in docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,10 @@ import { CitiesModule } from './modules/cities/cities.module';
     }),
     MongooseModule.forRoot(
       `mongodb://${process.env.ME_CONFIG_MONGODB_ADMINUSERNAME}:${process.env.ME_CONFIG_MONGODB_ADMINPASSWORD}@${process.env.ME_CONFIG_MONGODB_SERVER}:${process.env.ME_CONFIG_MONGODB_PORT}/${process.env.ME_CONFIG_MONGODB_NAME}?authSource=admin`,
+      {
+        useFindAndModify: false,
+        useNewUrlParser: true,
+      },
     ),
     CitiesModule,
   ],
--- src/modules/cities/open-weather/open-weather.service.ts
+++ src/modules/cities/open-weather/open-weather.service.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class OpenWeatherService {
   async getForecastByCityName(cityName: string): Promise<any> {
-    const apiKey = '9d572bdbde5fb9601ddb90ddc16a0053';
+    const apiKey = process.env.OPENWEATHER_KEY;
     const { data } = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
       { headers: { 'Content-Type': 'application/json' } },

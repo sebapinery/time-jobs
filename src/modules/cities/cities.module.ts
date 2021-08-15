cddc92a6ecdbe3d34948f97ddc35ae6bbde8e76a
sebapinery <sebapinery@gmail.com>
2021-08-14 22:03:49 -0300

    feat/ResponseTime Middleware added. fix/@types/mongoose deleted (unuseless)

--- src/modules/cities/cities.module.ts
+++ src/modules/cities/cities.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { City, CitySchema } from './schemas/city,schema';
 import { CitiesRepository } from './cities.repository';
 import { CitiesService } from './cities.service';
 import { CitiesController } from './cities.controller';
 import { OpenWeatherService } from './open-weather/open-weather.service';
+import { HelmetMiddleware } from '@nest-middlewares/helmet';
+import { ResponseTimeMiddleware } from '@nest-middlewares/response-time';
 
 @Module({
   imports: [
@@ -14,4 +21,11 @@ import { OpenWeatherService } from './open-weather/open-weather.service';
   exports: [],
   controllers: [CitiesController],
 })
-export class CitiesModule {}
+export class CitiesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    HelmetMiddleware.configure({});
+    consumer
+      .apply(ResponseTimeMiddleware)
+      .forRoutes({ path: 'cities', method: RequestMethod.GET });
+  }
+}

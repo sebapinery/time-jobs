747a71b0583d5405d5a466d03e6e2245a7bd260d
sebapinery <sebapinery@gmail.com>
2021-08-16 03:30:10 -0300

    feat/ catch error in OpenWheaterService in case City name is not found

--- src/cities/open-weather/open-weather.service.ts
+++ src/cities/open-weather/open-weather.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
 export class OpenWeatherService {
   async getForecastByCityName(cityName: string): Promise<any> {
     const apiKey = process.env.OPENWEATHER_KEY;
-    const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
-      { headers: { 'Content-Type': 'application/json' } },
-    );
-    return data;
+    const apiResponse = await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
+        { headers: { 'Content-Type': 'application/json' } },
+      )
+      .catch((err) => {
+        throw new HttpException('City not found', 404);
+      });
+
+    return apiResponse.data;
   }
 }

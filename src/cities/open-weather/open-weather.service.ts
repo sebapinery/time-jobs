3b8ae76b4dbe90557e308344b4507ffd242c8445
sebapinery <sebapinery@gmail.com>
2021-08-16 04:09:42 -0300

    fix/ destructing axios response

--- src/cities/open-weather/open-weather.service.ts
+++ src/cities/open-weather/open-weather.service.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export class OpenWeatherService {
   async getForecastByCityName(cityName: string): Promise<any> {
     const apiKey = process.env.OPENWEATHER_KEY;
-    const apiResponse = await axios
+    const { data } = await axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
         { headers: { 'Content-Type': 'application/json' } },
@@ -14,6 +14,6 @@ export class OpenWeatherService {
         throw new HttpException('City not found', 404);
       });
 
-    return apiResponse.data;
+    return data;
   }
 }

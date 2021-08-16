2456b5e1bc8988d34c043b7a242f4184789af83f
sebapinery <sebapinery@gmail.com>
2021-08-16 01:34:25 -0300

    delete unuseless comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CitiesModule } from './modules/cities/cities.module';
 
